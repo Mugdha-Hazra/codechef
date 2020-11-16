922944f0d84f3748d0ff775bfd2ca95a3bf4413e
Mugdha-Hazra <63424869+Mugdha-Hazra@users.noreply.github.com>
2020-11-16 22:35:18 +0530

    Contest Code : PRACTICE Problem Code:FLOW004

---
+++ undefined (4).cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+int main()
+{int t;
+cin>>t;
+while(t>0)
+{int p,q,s=0,n,r,c=0,d,a[100],i;
+cin>>n;
+p=n;
+i=0;
+while(n>0)
+{r=n%10;
+a[i]=r;
+n=n/10;
+i++;
+c++;
+}
+r=p%10;
+s=a[0]+a[c - 1];
+cout<<s<<"\n";
+t--;
+}
+return 0;
+}
