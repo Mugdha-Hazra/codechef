5375446577efa66cf9bd1fe525cbaf7aca94c8f6
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2020-12-15 08:08:01 +0530

    Create ADD TWO NUMBERS - FLOW001.cpp

---
+++ ADD TWO NUMBERS - FLOW001.cpp
@@ -0,0 +1,17 @@
+//https://www.codechef.com/problems/FLOW001
+#include<iostream>
+using namespace std;
+int main()
+{
+          int t;
+          cin>>t;
+          while(t>0)
+          {
+                    int a,b,c;
+                    cin>>a>>b;
+                    c=a+b;
+                    cout<<c<<"\n";
+                    t--;
+          }
+          return 0;
+}
