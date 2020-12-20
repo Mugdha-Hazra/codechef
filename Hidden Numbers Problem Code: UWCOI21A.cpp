130d1eafaa22ce0daac2bfa3fe03f9c9984142ea
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2020-12-20 09:32:17 +0530

    Create Hidden Numbers Problem Code: UWCOI21A.cpp

---
+++ Hidden Numbers Problem Code: UWCOI21A.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{int t;
+cin>>t;
+while(t>0)
+{int n;
+cin>>n;
+cout<<"1 "<<n;
+cout<<"\n";
+               t--;
+}	// your code goes here
+	return 0;
+}
