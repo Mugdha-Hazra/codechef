3a1502549676939ff044f56d5379c3b14bf30c4a
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2021-12-12 09:42:44 +0530

    Create INDIPERM.cpp

---
+++ INDIPERM.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        cout<<n<<" ";
+        for(int i=1;i<n;i++)
+        cout<<i<<" ";
+        cout<<"\n";
+        }
+    return 0;
+}
